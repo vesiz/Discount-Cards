cefe5f751746753c7cf678c72b782462240b9ce6
veselka zoneva <46721902+vesiz@users.noreply.github.com>
2019-11-28 20:28:04 +0200

    Delete DiscountCard.js

--- Entities/DiscountCard.js
+++
@@ -1,9 +0,0 @@
-class DiscountCard {
-    constructor(_customerEmail, _cardCode){
-        this.id = LocalStorage.updateCounter();
-        this.customerEmail = _customerEmail;
-        this.cardCode = _cardCode;
-        this.codeInfo = new CodeInfo(_cardCode);
-    }
-}
-
